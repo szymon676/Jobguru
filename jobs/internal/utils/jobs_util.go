beb1fdcfe4208218bbfc74e33de5eea3066a9ae5
Szymon <sgil73072@gmail.com>
2023-03-09 18:16:33 +0100

    improve verification

--- jobs/internal/utils/jobs_util.go
+++ jobs/internal/utils/jobs_util.go
@@ -30,7 +30,11 @@ func WriteJSON(w http.ResponseWriter, status int, v any) error {
 }
 
 func VerifyJSON(bj models.BindJob) error {
-	if bj.Category == "" || bj.Title == "" || bj.Description == "" {
+	if bj.Category == "" || bj.Title == "" || bj.Description == "" || bj.Skills == nil {
+		return fmt.Errorf("error binding job")
+	}
+
+	if len(bj.Skills) < 1 || len(bj.Category) < 3 || len(bj.Title) < 3 || len(bj.Description) < 10 {
 		return fmt.Errorf("error binding job")
 	}
 	return nil
