92aa688f0ff712cd1bce806e5fdd2d0e28c63235
JustKooba <jakubsakw2009@gmail.com>
2023-04-04 20:34:29 +0200

    made the dark mode toggle working

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -7,7 +7,7 @@
     </div>
 
     <form>
-      <button class="darkModeBtn" @click="darkModeToggle">
+      <button class="darkModeBtn" @click.prevent="darkModeToggle">
         <img src="../assets/icon-moon-light.svg" alt="a moon icon" class="darkModeImg" />
       </button>
       <input type="text" placeholder="e.g. Junior java developer" />
@@ -21,24 +21,30 @@ import { defineComponent, onMounted } from 'vue'
 
 export default defineComponent({
   name: 'HeaderComponent',
-  setup() {
-    const darkModeToggle = () => {
+
+  methods: {
+    darkModeToggle() {
       const darkModeBtn = document.querySelector('.darkModeBtn')
-      const header = document.querySelector('.header')
+      const darkModeImg = document.querySelector('.darkModeImg')
       const body = document.querySelector('body')
+      const header = document.querySelector('.header')
+      const input = document.querySelector('input')
+      const homeIcon = document.querySelector('.left img')
+      const bellIcon = document.querySelector('.left img:nth-child(2)')
+      const magnifierIcon = document.querySelector('button:nth-child(3) img')
+      const jobGuru = document.querySelector('.header h2')
 
       darkModeBtn?.addEventListener('click', () => {
-        header?.classList.toggle('darkToggle')
-        body?.classList.toggle('darkToggle')
+        darkModeBtn.classList.toggle('DarkToggle')
+        darkModeImg?.classList.toggle('DarkToggle')
+        body?.classList.toggle('DarkToggle')
+        header?.classList.toggle('DarkToggle')
+        input?.classList.toggle('DarkToggle')
+        homeIcon?.classList.toggle('DarkToggle')
+        bellIcon?.classList.toggle('DarkToggle')
+        magnifierIcon?.classList.toggle('DarkToggle')
+        jobGuru?.classList.toggle('DarkToggle')
       })
-    }
-
-    onMounted(() => {
-      darkModeToggle()
-    })
-
-    return {
-      darkModeToggle
     }
   }
 })
@@ -111,7 +117,22 @@ export default defineComponent({
   cursor: pointer;
 }
 
+.darkModeBtn {
+  background-color: #1f1f1f;
+  border: none;
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+  width: 50px;
+  margin-right: 20px;
+  outline: none;
+  cursor: pointer;
+}
+
 .DarkToggle {
   background-color: #fff !important;
+  color: #121213 !important;
 }
 </style>
